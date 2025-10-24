53721ea018b26d02e0dbf0d40adf98b3a420dc38
KingJMS1 <1kingjms@gmail.com>
2025-10-24 23:37:16 +0000

    Mess with slurm again

--- experiments/convergence_analysis/run_model.sh
+++ experiments/convergence_analysis/run_model.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=10
-#SBATCH --nodes=25
-#SBATCH --ntasks=100
-#SBATCH --mem=64G
+#SBATCH --mem-per-cpu=800M
 #SBATCH --time=24:00:00
-#SBATCH --output=conv_analysis_model_run_%j.txt
+#SBATCH --output=conv_analysis_model_run_%j_%a.txt
 #SBATCH --job-name=conv_analysis_model
+#SBATCH --array=1-100
 
 module load anaconda
 conda activate renv
 
-srun --multi-prog run_model.conf
+srun Rscript model_runner.R ${SLURM_ARRAY_TASK_ID}
