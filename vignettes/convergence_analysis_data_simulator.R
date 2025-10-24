7b6f05587bcda19dc5d98f6e226a02cdc22c9a52
KingJMS1 <1kingjms@gmail.com>
2025-10-24 21:21:14 +0000

    Add convergence analysis model runner

--- vignettes/convergence_analysis_data_simulator.R
+++ vignettes/convergence_analysis_data_simulator.R
@@ -74,7 +74,7 @@ for (obs_num in obs_nums)
         num_temporal <- 10
 
         # Get Spatial and temporal design matrices, and total number of observations
-        out <- make_Vs_Vt(num_spatial, num_temporal, 50)
+        out <- make_Vs_Vt(num_spatial, num_temporal, obs_num)
         Vs <- out$Vs
         Vt <- out$Vt
         N  <- out$N
@@ -139,39 +139,3 @@ for (obs_num in obs_nums)
         save(list=c("X", "y", "coords", "Vs", "Vt", "Ds", "Dt", "u", "a", "b", "c", "d"), file=paste("simulations/ob_", obs_num, "_rep_", kk, ".rda", sep=""))
     }
 }
-
-
-
-
-# #################
-# # Run the Model #
-# #################
-# # Run for a short time for demo purposes
-# cat("Running Model\n")
-# output <- ZINB_GP(X, y, coords, Vs, Vt, Ds, Dt, M = 10, 1000, 200, 1, TRUE, print_progress = TRUE)
-# predictions <- output$Y_pred
-# sim_alpha <- output$Alpha
-# sim_beta <- output$Beta
-# sim_spatial_noise1 <- output$Noise1s
-
-# # Examine coefficients for regressions
-# cat("\nLogistic Regression Coefficients:\n")
-# alpha
-# apply(sim_alpha, 2, function(x) quantile(x, probs=c(0.025, 0.5, 0.975)))
-
-# cat("\nNegative Binomial Coefficients:\n")
-# beta
-# apply(sim_beta, 2, function(x) quantile(x, probs=c(0.025, 0.5, 0.975)))
-
-# # Examine how often various samples are at risk
-# cat("\nAt risk 'probabilities':\n")
-# at_risk <- output$at_risk
-# sim_p_at_risk <- apply(at_risk, 2, mean)
-# sim_p_at_risk[1:20]
-# cat("\nActual at risk:\n")
-# u[1:20]
-
-# cat("\nEstimated Spatial Noise:\n")
-# mean(sim_spatial_noise1)
-# cat("\nActual Spatial Noise:\n")
-# spatial_noise
