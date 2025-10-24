19f5b1967d4b6f548a97090f027a87285721caf5
KingJMS1 <1kingjms@gmail.com>
2025-10-24 23:15:03 +0000

    Tweak slurm code so we don't take forever

--- experiments/convergence_analysis/run_model.sh
+++ experiments/convergence_analysis/run_model.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=4
-#SBATCH --nodes=10
+#SBATCH --cpus-per-task=10
+#SBATCH --nodes=25
 #SBATCH --ntasks=100
 #SBATCH --mem=64G
-#SBATCH --time=8:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=conv_analysis_model_run_%j.txt
 #SBATCH --job-name=conv_analysis_model
 
